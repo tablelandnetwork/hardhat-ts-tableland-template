e1313b63e1ed335b972617e68c85df066444228b
Dan Buchholz <dbuchholz30@gmail.com>
2023-07-04 11:46:22 -0700

    initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,169 @@
+import * as dotenv from "dotenv";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@openzeppelin/hardhat-upgrades";
+import "@tableland/hardhat";
+import { HardhatUserConfig } from "hardhat/config";
+import "hardhat-dependency-compiler";
+dotenv.config();
+
+const homestead = {
+  url: `https://eth-mainnet.alchemyapi.io/v2/${
+    process.env.ETHEREUM_API_KEY ?? ""
+  }`,
+  accounts:
+    process.env.ETHEREUM_PRIVATE_KEY !== undefined
+      ? [process.env.ETHEREUM_PRIVATE_KEY]
+      : [],
+};
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  dependencyCompiler: {
+    paths: [
+      // For unit testing purposes, the registry contract must be deployed
+      "@tableland/evm/contracts/TablelandTables.sol",
+    ],
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  localTableland: {
+    silent: false,
+    verbose: false,
+  },
+  etherscan: {
+    apiKey: {
+      // ethereum
+      mainnet: process.env.ETHERSCAN_API_KEY || "",
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
+      // optimism
+      optimisticEthereum: process.env.OPTIMISM_ETHERSCAN_API_KEY || "",
+      optimisticGoerli: process.env.OPTIMISM_ETHERSCAN_API_KEY || "",
+      // arbitrum
+      arbitrumOne: process.env.ARBISCAN_API_KEY || "",
+      arbitrumNova: process.env.ARBISCAN_NOVA_API_KEY || "",
+      arbitrumGoerli: process.env.ARBISCAN_API_KEY || "",
+      // polygon
+      polygon: process.env.POLYGONSCAN_API_KEY || "",
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
+    },
+    customChains: [
+      {
+        network: "arbitrumNova",
+        chainId: 42170,
+        urls: {
+          apiURL: "https://api-nova.arbiscan.io/api",
+          browserURL: "https://nova.arbiscan.io/",
+        },
+      },
+    ],
+  },
+  networks: {
+    // mainnets
+    mainnet: homestead,
+    homestead,
+    optimism: {
+      url: `https://opt-mainnet.g.alchemy.com/v2/${
+        process.env.OPTIMISM_API_KEY ?? ""
+      }`,
+      accounts:
+        process.env.OPTIMISM_PRIVATE_KEY !== undefined
+          ? [process.env.OPTIMISM_PRIVATE_KEY]
+          : [],
+    },
+    arbitrum: {
+      url: `https://arb-mainnet.g.alchemy.com/v2/${
+        process.env.ARBITRUM_API_KEY ?? ""
+      }`,
+      accounts:
+        process.env.ARBITRUM_PRIVATE_KEY !== undefined
+          ? [process.env.ARBITRUM_PRIVATE_KEY]
+          : [],
+    },
+    "arbitrum-nova": {
+      url: `https://skilled-yolo-mountain.nova-mainnet.discover.quiknode.pro/${
+        process.env.ARBITRUM_NOVA_API_KEY ?? ""
+      }`,
+      accounts:
+        process.env.ARBITRUM_NOVA_PRIVATE_KEY !== undefined
+          ? [process.env.ARBITRUM_NOVA_PRIVATE_KEY]
+          : [],
+    },
+    matic: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${
+        process.env.POLYGON_API_KEY ?? ""
+      }`,
+      accounts:
+        process.env.POLYGON_PRIVATE_KEY !== undefined
+          ? [process.env.POLYGON_PRIVATE_KEY]
+          : [],
+    },
+    filecoin: {
+      url: `https://rpc.ankr.com/filecoin/${
+        process.env.FILECOIN_API_KEY ?? ""
+      }`,
+      accounts:
+        process.env.FILECOIN_PRIVATE_KEY !== undefined
+          ? [process.env.FILECOIN_PRIVATE_KEY]
+          : [],
+    },
+    // testnets
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${
+        process.env.ETHEREUM_SEPOLIA_API_KEY ?? ""
+      }`,
+      accounts:
+        process.env.ETHEREUM_SEPOLIA_PRIVATE_KEY !== undefined
+          ? [process.env.ETHEREUM_SEPOLIA_PRIVATE_KEY]
+          : [],
+    },
+    "optimism-goerli": {
+      url: `https://opt-goerli.g.alchemy.com/v2/${
+        process.env.OPTIMISM_GOERLI_API_KEY ?? ""
+      }`,
+      accounts:
+        process.env.OPTIMISM_GOERLI_PRIVATE_KEY !== undefined
+          ? [process.env.OPTIMISM_GOERLI_PRIVATE_KEY]
+          : [],
+    },
+    "arbitrum-goerli": {
+      url: `https://arb-goerli.g.alchemy.com/v2/${
+        process.env.ARBITRUM_GOERLI_API_KEY ?? ""
+      }`,
+      accounts:
+        process.env.ARBITRUM_GOERLI_PRIVATE_KEY !== undefined
+          ? [process.env.ARBITRUM_GOERLI_PRIVATE_KEY]
+          : [],
+    },
+    maticmum: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${
+        process.env.POLYGON_MUMBAI_API_KEY ?? ""
+      }`,
+      accounts:
+        process.env.POLYGON_MUMBAI_PRIVATE_KEY !== undefined
+          ? [process.env.POLYGON_MUMBAI_PRIVATE_KEY]
+          : [],
+    },
+    "filecoin-calibration": {
+      url: `https://api.calibration.node.glif.io/rpc/v1${
+        process.env.FILECOIN_CALIBRATION_API_KEY ?? ""
+      }`,
+      accounts:
+        process.env.FILECOIN_CALIBRATION_PRIVATE_KEY !== undefined
+          ? [process.env.FILECOIN_CALIBRATION_PRIVATE_KEY]
+          : [],
+    },
+  },
+};
+
+export default config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const Starter = await ethers.getContractFactory("Starter");
+  const starter = await Starter.deploy();
+
+  await starter.deployed();
+  console.log(`Contract deployed to '${starter.address}'.\n`);
+
+  const tableName = await starter.tableName();
+  console.log(`Table name '${tableName}' minted to contract.`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
